7291997699e5667360d723e36e8f4eccb0262afb
alexandershubin <shubin8@mail.ru>
2020-02-24 17:22:26 +0300

    add new style

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -9,31 +9,34 @@ export const Filter = ({
 }) => (
   <form className="form" action="#" method="post">
     <ul className="form__list">
-      <li className="form__item">
+      <li className="form__item form__item--range">
         <label htmlFor="cores">vCPU</label>
         <div className="output">
-          <input className="form__item--slider" type="range" id="cores" name="range" min={2} max={12} step={2}
+          <input className="form__slider" type="range" id="cores" name="range" min={2} max={12} step={2}
                  defaultValue={range} onInput={handleFilterChange}/>
           <div className="output__count">
             <span>2</span>
-            <span>{range}</span>
+            <div className="output__wrap">
+              <output name="count">{range}</output>
+              <span> ядер</span>
+            </div>
             <span>12</span>
           </div>
         </div>
       </li>
-      <li className="form__item">
+      <li className="form__item form__item--checkbox">
         <input checked={gpu} onChange={handleFilterChange} className="form__item form__item--one"
                type="checkbox" name="gpu"
                id="filter-one"/>
         <label htmlFor="filter-one">GPU</label>
       </li>
-      <li className="form__item">
+      <li className="form__item form__item--checkbox">
         <input checked={raid} onChange={handleFilterChange} className="form__item form__item--two"
                type="checkbox" name="raid"
                id="filter-two"/>
         <label htmlFor="filter-two">RAID</label>
       </li>
-      <li className="form__item">
+      <li className="form__item form__item--checkbox">
         <input checked={ssd} onChange={handleFilterChange} className="form__item form__item--three"
                type="checkbox" name="ssd"
                id="filter-three"/>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+const prettify = (num) => {
+  let n = num.toString();
+  return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+};
+
 export const ListItem = ({item}) => (
   <li className="product__list">
     <div className="product__first">
       <h3>{item.name}</h3>
       <div>
         <span>{item.cpu.count}</span> x {item.cpu.name}
-        <span>{item.cpu.count * item.cpu.cores < 5 ? item.cpu.count * item.cpu.cores + ' ядра' : item.cpu.count * item.cpu.cores + ' ядер'}</span>
+        <span>{item.cpu.count * item.cpu.cores < 5 ? ' ' + item.cpu.count * item.cpu.cores + ' ядра' : ' ' + item.cpu.count * item.cpu.cores + ' ядер'}</span>
       </div>
     </div>
     <div className="product__second">
@@ -18,7 +23,7 @@ export const ListItem = ({item}) => (
       <span>{item.gpu ? item.gpu : ''}</span>
     </div>
     <div className="product__right">
-      <h3>{Math.floor(item.price)} ₽/месяц</h3>
+      <h3>{prettify(item.price / 100)} ₽/месяц</h3>
       <button className="product__button button">Заказать</button>
     </div>
   </li>
